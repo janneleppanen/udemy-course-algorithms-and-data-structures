5c2df350864fe03f4d7c286ef352744a48c7febb
Janne Leppänen <janne@janneleppanen.com>
2020-01-01 09:21:57 +0200

    Solutions for reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,37 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  var reversedString = "";
+
+  for (var i = 0; i < str.length; i++) {
+    reversedString = str[i] + reversedString;
+  }
+
+  return reversedString;
+}
+
+function reverse2(str) {
+  return str
+    .split("")
+    .reverse()
+    .join("");
+}
+
+function reverse3(str) {
+  let reversed = "";
+
+  for (character of str) {
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+function reverse4(str) {
+  return str
+    .split("")
+    .reduce((reversed, character) => character + reversed, "");
+}
 
 module.exports = reverse;
