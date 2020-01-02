4b0f90592534da4f2dbef96766f15fb556aa07e3
Janne Leppänen <janne@janneleppanen.com>
2020-01-02 20:06:25 +0200

    Solutions for anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const sortLetters = str =>
+    str
+      .replace(/\W/g, "")
+      .toLowerCase()
+      .split("")
+      .sort()
+      .join("");
+
+  return sortLetters(stringA) === sortLetters(stringB);
+}
+
+function anagrams2(stringA, stringB) {
+  function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/\W/g, "").toLowerCase()) {
+      charMap[char] = charMap[char] + 1 || 1;
+    }
+
+    return charMap;
+  }
+
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length)
+    return false;
+
+  for (let letter in charMapA) {
+    if (charMapA[letter] !== charMapB[letter]) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
