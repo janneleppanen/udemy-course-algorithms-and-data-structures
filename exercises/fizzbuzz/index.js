5dd0afb01d323fde06e22d10320f247ff73d8149
Janne Leppänen <janne@janneleppanen.com>
2020-01-02 18:40:28 +0200

    Add solution for fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,38 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let display = "";
+
+    if (i % 3 === 0) {
+      display += "fizz";
+    }
+
+    if (i % 5 === 0) {
+      display += "buzz";
+    }
+
+    if (display === "") {
+      display = i;
+    }
+
+    console.log(display);
+  }
+}
+
+function fizzBuzz1(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("fizzbuzz");
+    } else if (i % 3 === 0) {
+      console.log("fizz");
+    } else if (i % 5 === 0) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 module.exports = fizzBuzz;
