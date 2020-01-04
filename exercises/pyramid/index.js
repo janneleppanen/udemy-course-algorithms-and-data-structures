41129c3df27b123d0f958074807117bf5cc033be
Janne Leppänen <janne@janneleppanen.com>
2020-01-04 16:20:01 +0200

    Solutions for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const width = n * 2 - 1;
+  const mid = Math.floor(width / 2);
+
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let col = 0; col < width; col++) {
+      if (mid - row <= col && mid + row >= col) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+function pyramid2(n, row = 0, level = "") {
+  const width = n * 2 - 1;
+  const mid = Math.floor(width / 2);
+  const col = level.length;
+
+  if (row >= n) {
+    return;
+  }
+
+  if (level.length === width) {
+    console.log(level);
+    return pyramid(n, row + 1, "");
+  }
+
+  if (mid - row <= col && mid + row >= col) {
+    level += "#";
+  } else {
+    level += " ";
+  }
+
+  pyramid(n, row, level);
+}
 
 module.exports = pyramid;
