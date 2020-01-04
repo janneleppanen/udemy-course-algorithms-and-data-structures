60d10b0c2483913077efe1596e7acf8e1093ba22
Janne Leppänen <janne@janneleppanen.com>
2020-01-04 14:54:21 +0200

    Solutions for steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,54 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  for (let i = 0; i < n; i++) {
+    let level = "";
+
+    for (let j = 0; j < n; j++) {
+      if (j <= i) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+
+    console.log(level);
+  }
+}
+
+function steps2(n, row = 0) {
+  if (row >= n) {
+    return;
+  }
+
+  let level = "";
+
+  for (let i = 0; i < n; i++) {
+    if (i <= row) {
+      level += "#";
+    } else {
+      level += " ";
+    }
+  }
+
+  console.log(level);
+  steps(n, row + 1);
+}
+
+function steps3(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  stair += stair.length <= row ? "#" : " ";
+
+  steps(n, row, stair);
+}
 
 module.exports = steps;
