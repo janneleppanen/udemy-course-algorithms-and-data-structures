9aabf24a4346152a1df6419937ef1f472442c2c5
Janne Leppänen <janne@janneleppanen.com>
2020-01-09 19:04:01 +0200

    Solution for queue from stacks

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,45 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.fillStack = new Stack();
+    this.removeStack = new Stack();
+  }
+
+  add(item) {
+    this.fillStack.push(item);
+  }
+
+  remove() {
+    while (this.fillStack.peek()) {
+      this.removeStack.push(this.fillStack.pop());
+    }
+
+    const itemToRemove = this.removeStack.pop();
+
+    while (this.removeStack.peek()) {
+      this.fillStack.push(this.removeStack.pop());
+    }
+
+    return itemToRemove;
+  }
+
+  peek() {
+    while (this.fillStack.peek()) {
+      this.removeStack.push(this.fillStack.pop());
+    }
+
+    const itemToPeek = this.removeStack.peek();
+
+    while (this.removeStack.peek()) {
+      this.fillStack.push(this.removeStack.pop());
+    }
+
+    return itemToPeek;
+  }
+}
 
 module.exports = Queue;
